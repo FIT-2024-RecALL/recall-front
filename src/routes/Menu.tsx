987a3dc74c3b6bb04b572416669f661b71ea93f6
Kirill Kozlov <kozlov.kirill.v.work@gmail.com>
2024-10-12 17:39:46 +0700

    Add colors and main classes for them, set font

--- src/routes/Menu.tsx
+++ src/routes/Menu.tsx
@@ -5,10 +5,10 @@ import { Button } from '@/components/library/Button';
 
 export const Menu: React.FC = () => {
   const links = routesList.map((data) => (
-    <Button variant="green" className={'p-2 m-2'} key={data.url}>
+    <Button variant="green" className="p-2 m-2" key={data.url}>
       <Link to={data.url}>{data.label}</Link>
     </Button>
   ));
 
-  return <header className={'fixed flex'}>{links}</header>;
+  return <header className="fixed flex">{links}</header>;
 };
