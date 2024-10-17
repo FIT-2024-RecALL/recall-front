98b597186217c8d48b17a512276753ac0051b0ba
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2024-10-17 10:23:38 +0700

    Change pop-up login start corner and add docs for pop-up window

--- src/components/library/PopUp.tsx
+++ src/components/library/PopUp.tsx
@@ -6,6 +6,17 @@ interface PopUpProps extends PropsWithChildren<React.HTMLAttributes<React.FC>> {
   close: () => void;
 }
 
+/**
+ * Pop-up window wrapper FC.
+ * 
+ * - `isShown` - should content be rendered
+ * - `close` - callback for closing
+ * - `className` - extenstion for pop-up css-classes
+ *   - Pop-up wrapper doesn't have any visible styles for itself by default so you can add them manually
+ *   - Css-parameters like "display: hidden" may cause animtation to be broken
+ * - `children` - pop-up window's content
+ * - other parameters will be forwarded to the wrapper div tag 
+ */
 export const PopUp: React.FC<PopUpProps> = (
   { isShown, close, className, children },
   ...props
