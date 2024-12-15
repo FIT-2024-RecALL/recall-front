c737a448ddab789484be486fffbed461919613c4
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2024-12-15 17:38:59 +0700

    Small fix for start page

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -11,8 +11,12 @@ export const StartPage: React.FC = () => {
   return (
     <div className="vstack m-2 md:m-10 p-2 md:p-5 text-o-black rounded-md">
       <h1 className="text-4xl text-center my-6 font-bold">
-        Make engrams for everything you want
+        Here{"'"}s so simple to recall anything
       </h1>
+      <h2 className="text-4xl text-center my-4 font-bold">
+        Create collections by any topic, add cards with text, photos, audios or
+        videos and train them
+      </h2>
       <div className="w-full center vstack">
         <Button
           variant="plate"
