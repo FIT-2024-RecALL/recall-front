9adf5b8228d8c06f30d4be39994b5b625cffb65a
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2024-12-17 14:48:25 +0700

    Some code optimization

--- src/pages/CollectionsPage.tsx
+++ src/pages/CollectionsPage.tsx
@@ -6,30 +6,11 @@ import { useCollections, useProfile } from '@/query/queryHooks';
 import { CollectionShort } from '@/api';
 import { useAppStore } from '@/state';
 import clsx from 'clsx';
+import { CollectionsSearchableList } from '@/components/collection/CollectionsSearchableList';
+import { LoadableComponent } from '../components/library/LoadableComponent';
 
 export const CollectionsPage: React.FC = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const setIsCreateCollectionOpened = useAppStore(
-    (state) => state.setIsCreateCollectionWindowOpened
-  );
-
-  const { profile } = useProfile();
-  const { collections } = useCollections();
-  const [activeCollections, setAcitveCollections] = useState<CollectionShort[]>(
-    []
-  );
-
-  useEffect(() => {
-    if (!collections) return;
-    const filteredCollections =
-      searchTerm.trim() === ''
-        ? collections
-        : collections.filter((item) =>
-            item.title.toLowerCase().includes(searchTerm.toLowerCase())
-          );
-    setAcitveCollections(filteredCollections);
-  }, [searchTerm, collections]);
+  const { collections, isPending, error } = useCollections();
 
   return (
     <div className="flex flex-col items-center m-4 md:m-10 p-5 text-o-black">
@@ -37,45 +18,9 @@ export const CollectionsPage: React.FC = () => {
         Collections
       </h1>
 
-      {profile && (
-        <div className="flex justify-center mb-4">
-          <Button
-            variant="plate"
-            className={clsx(
-              'py-3 px-6 rounded-full',
-              'text-lg font-medium',
-              'shadow-md hover:shadow-lg',
-              'transition duration-200'
-            )}
-            onClick={() => setIsCreateCollectionOpened(true)}
-          >
-            Create collection
-          </Button>
-        </div>
-      )}
-
-      <SearchBar
-        searchTerm={searchTerm}
-        setSearchTerm={setSearchTerm}
-        activeSearch={activeCollections.map((item) => item.title)}
-      />
-
-      <div
-        className="grid align-center justify-center w-full gap-4 pt-4"
-        style={{
-          gridTemplateColumns: 'repeat( auto-fit, 320px )',
-        }}
-      >
-        {activeCollections.length > 0 ? (
-          activeCollections.map((item) => (
-            <CollectionCard key={item.id} collectionId={item.id} />
-          ))
-        ) : (
-          <p className="text-center text-o-white col-span-full">
-            No collections found
-          </p>
-        )}
-      </div>
+      <LoadableComponent isPending={isPending} errorMessage={error?.message}>
+        {collections && <CollectionsSearchableList collections={collections} />}
+      </LoadableComponent>
     </div>
   );
 };
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -15,45 +15,13 @@ import { useAppStore } from '@/state';
 import { ErrorPage } from './ErrorPage';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { dataExtractionWrapper } from '@/query';
+import { FilesList } from '../components/profile/FilesList';
+import { CollectionsSearchableList } from '@/components/collection/CollectionsSearchableList';
 
 export const ProfilePage: React.FC = () => {
   const { profile, isPending: isProfilePending } = useProfile();
   const { collections, isPending: isCollectionsPending } =
     useProfileCollections();
-  const { files, isPending: isFilesPending } = useFilesList();
-
-  const client = useQueryClient();
-  const { mutate: deleteFile } = useMutation({
-    mutationFn: (filename: string) =>
-      dataExtractionWrapper(
-        deleteFileStorageFilenameDelete({ path: { filename } })
-      ),
-    onSuccess: () => {
-      client.invalidateQueries({
-        queryKey: getFilesListQueryOptions().queryKey,
-      });
-    },
-  });
-
-  const setIsCreateCollectionOpened = useAppStore(
-    (state) => state.setIsCreateCollectionWindowOpened
-  );
-
-  const [searchTerm, setSearchTerm] = useState('');
-  const [activeCollections, setAcitveCollections] = useState<CollectionShort[]>(
-    []
-  );
-
-  useEffect(() => {
-    if (!collections) return;
-    const filteredCollections =
-      searchTerm.trim() === ''
-        ? collections
-        : collections.filter((item) =>
-            item.title.toLowerCase().includes(searchTerm.toLowerCase())
-          );
-    setAcitveCollections(filteredCollections);
-  }, [searchTerm, collections]);
 
   if (!profile)
     return (
@@ -66,7 +34,7 @@ export const ProfilePage: React.FC = () => {
   return (
     <LoadableComponent
       className="flex flex-col items-center m-4 md:m-10 p-5"
-      isPending={isProfilePending || isCollectionsPending || isFilesPending}
+      isPending={isProfilePending || isCollectionsPending}
       animated
     >
       <h1 className="text-center text-2xl font-bold mb-6">
@@ -78,94 +46,13 @@ export const ProfilePage: React.FC = () => {
 
       <h2 className="text-center text-2xl font-bold mb-6">Your collections</h2>
 
-      <div className="flex justify-center mb-4">
-        <Button
-          variant="plate"
-          className={clsx(
-            'py-3 px-6 rounded-full',
-            'text-lg font-medium',
-            'shadow-md hover:shadow-lg',
-            'transition duration-200'
-          )}
-          onClick={() => setIsCreateCollectionOpened(true)}
-        >
-          Create collection
-        </Button>
-      </div>
-
-      {collections && collections.length == 0 && (
-        <h3 className="text-center text-xl font-medium col-span-full">
-          There{"'"}re no collections yet. Create the first!
-        </h3>
-      )}
-      {collections && collections.length > 0 && (
-        <>
-          <SearchBar
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-            activeSearch={activeCollections.map((item) => item.title)}
-          />
-
-          <div
-            className="grid align-center justify-center w-full gap-4 pt-4"
-            style={{
-              gridTemplateColumns: 'repeat( auto-fit, 320px )',
-            }}
-          >
-            {activeCollections.length > 0 ? (
-              activeCollections.map((item) => (
-                <CollectionCard key={item.id} collectionId={item.id} />
-              ))
-            ) : (
-              <h3 className="text-center text-xl font-medium col-span-full">
-                No collections found. Maybe create new one?
-              </h3>
-            )}
-          </div>
-        </>
-      )}
+      {collections && <CollectionsSearchableList collections={collections} />}
 
       <hr className="border-2 border-1-1 rounded my-2 md:my-6 w-full" />
 
       <h2 className="text-center text-2xl font-bold mb-6">Your files</h2>
 
-      <div className="grid grid-cols-1 align-center justify-center w-full gap-4">
-        {files && files.length === 0 && (
-          <h3 className="text-center text-xl font-medium col-span-full">
-            You haven{"'"}t upload any file yes. Feel free to add them using
-            card{"'"}s edit form
-          </h3>
-        )}
-        {files &&
-          files.length > 0 &&
-          files?.map((file) => (
-            <div
-              className={clsx(
-                'bg-1-8 px-2 py-4',
-                'grid grid-cols-4',
-                'gap-x-4 rounded-lg'
-              )}
-              key={file.url}
-            >
-              <div className="col-span-4 md:col-span-3 truncate">
-                <a className="w-fit" href={getFileFullPath(file.url)}>
-                  <Button className="w-full" variant="inline">
-                    {getFileFullPath(file.url)}
-                  </Button>
-                </a>
-              </div>
-              <div className="col-span-4 md:col-span-1">
-                <Button
-                  className="w-full"
-                  variant="bordered-trans"
-                  onClick={() => deleteFile(file.filename)}
-                >
-                  Delete file
-                </Button>
-              </div>
-            </div>
-          ))}
-      </div>
+      <FilesList />
     </LoadableComponent>
   );
 };
