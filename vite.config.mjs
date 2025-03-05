337023b11525b8d87aade2428067321414b472da
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2025-03-05 23:13:52 +0700

    Next try

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,10 +3,11 @@ import { default as viteReact } from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import svgr from 'vite-plugin-svgr';
 import tailwindcss from '@tailwindcss/vite';
+import commonjs from 'vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteReact(), svgr(), tailwindcss()],
+  plugins: [viteReact(), svgr(), tailwindcss(), commonjs()],
   server: {
     strictPort: true,
   },
