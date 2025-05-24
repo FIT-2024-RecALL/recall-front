e9a4350b9c616fb8b5efe896aa8a05df2183c109
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2025-05-24 16:57:57 +0700

    Another one shadow wrapper fix

--- src/components/library/ShadowWrapper.tsx
+++ src/components/library/ShadowWrapper.tsx
@@ -1,8 +1,29 @@
 import React, { HTMLAttributes, useMemo } from 'react';
 import clsx from 'clsx';
 
+const OFFSETS = {
+  1: [
+    'md:hover:-translate-x-1',
+    'md:hover:-translate-y-1',
+    'md:hover:before:translate-x-1',
+    'md:hover:before:translate-y-1',
+  ],
+  2: [
+    'md:hover:-translate-x-2',
+    'md:hover:-translate-y-2',
+    'md:hover:before:translate-x-2',
+    'md:hover:before:translate-y-2',
+  ],
+  3: [
+    'md:hover:-translate-x-3',
+    'md:hover:-translate-y-3',
+    'md:hover:before:translate-x-3',
+    'md:hover:before:translate-y-3',
+  ],
+};
+
 export interface ShadowWrapperProps extends HTMLAttributes<React.FC> {
-  shadowOffset?: number;
+  shadowOffset?: keyof typeof OFFSETS;
   bgClass?: string;
 }
 
@@ -12,15 +33,6 @@ export const ShadowWrapper: React.FC<ShadowWrapperProps> = ({
   bgClass,
   children,
 }) => {
-  const offsetClasses = useMemo(() => {
-    const offset = shadowOffset ?? 1;
-    return [
-      `md:hover:-translate-x-${offset}`,
-      `md:hover:-translate-y-${offset}`,
-      `md:hover:before:translate-x-${offset}`,
-      `md:hover:before:translate-y-${offset}`
-    ]
-  }, [shadowOffset]);
   const bgColorClass = useMemo(
     () => (bgClass ? `before:${bgClass}` : 'before:bg-black/50'),
     [bgClass]
@@ -33,7 +45,7 @@ export const ShadowWrapper: React.FC<ShadowWrapperProps> = ({
         'before:absolute before:full before:rounded-md',
         bgColorClass,
         'before:transition-all before:duration-200',
-        offsetClasses,
+        OFFSETS[shadowOffset || 1],
         className
       )}
     >
